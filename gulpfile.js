530ba326c4f94faa0f3ef3eec9b32a81a515c196
John Pittman <johnrichadrpittman@gmail.com>
2014-11-14 18:08:51 -0500

    Fixed .min file names after source mapping. Added to gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 'use strict'
 
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglifyjs');
 var jasmine = require('gulp-jasmine');
-var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
 
 /**
  * Setup
@@ -11,43 +12,60 @@ var sourcemaps = require('gulp-sourcemaps');
 
 // File paths.
 var JS_SRC = 'src/**/*.js';
-var DIST = 'dist';
+var DIST = 'dist/';
 var SPECS = 'test/*Spec.js';
-
-// Setup to terminate the deploy process on task errors.
-var exitCode = 0;
-
-gulp.on('err', function(err) {
-    process.nextTick(function() {
-        process.exit(exitCode)
-    })
-});
 
 /**
  * Tasks
  */
 
-gulp.task('default', ['test', 'build']);
+// Group tasks by development process.
+gulp.task('default', []);
+gulp.task('test', ['unitSpecs']);
+gulp.task('build', ['cleanDotMinFiles']);
 
-// Minify JS
-gulp.task('compress', function() {
+// Clear the distribution directory in case files/folders were removed from last build.
+gulp.task('cleanDist', ['unitSpecs'], function() {
+    return gulp.src(DIST, {read: false})
+        .pipe(clean())
+        .pipe(gulp.dest('./'));
+});
+
+// Minify JS with source maps
+gulp.task('compress', ['cleanDist'], function() {
     return gulp.src(JS_SRC)
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
+        .pipe(rename(function(path) {
+                path.extname = '.min'
+        }))
+        .pipe(uglify({
+            outSourceMap: true
+        }))
         .pipe(gulp.dest(DIST));
 });
 
-// Test JS
+// Put the .js back onto the minified files after compressed with source maps.
+gulp.task('rename', ['compress'], function() {
+    return gulp.src(DIST + '*.min')
+        .pipe(rename(function(path) {
+            path.extname = '.min.js'
+        }))
+        .pipe(gulp.dest(DIST));
+});
+
+// Remove all .min files since they copied as .min.js.
+gulp.task('cleanDotMinFiles', ['rename'], function() {
+    return gulp.src(DIST + '*.min', {read: false})
+        .pipe(clean())
+        .pipe(gulp.dest(DIST));
+});
+
+// Run all units tests.
 gulp.task('unitSpecs', function() {
     return gulp.src(SPECS)
         .pipe(jasmine())
         .on('close', function(code, signal) {
-            if (code) 
-            	exitCode = code;
+            if (code)
+                exitCode = code;
             done();
         });
 });
-
-gulp.task('build', ['compress']);
-gulp.task('test', ['unitSpecs']);
